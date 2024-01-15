c1c6d26d92b6dae5ef72b472e2c8abf8cf1f81cb
Ivan Sversky <codethegit@gmail.com>
2024-01-15 20:07:48 +0800

    update

--- .config/hypr/scripts/nogaps.sh
+++ .config/hypr/scripts/nogaps.sh
@@ -1,16 +1,24 @@
-#!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption general:gaps_in | awk 'NR==2{print $2}')
-if [ "$HYPRGAMEMODE" = 6 ] ; then
- hyprctl --batch "\
-     keyword general:gaps_in 0;\
-     keyword general:gaps_out 0;\
-     keyword general:border_size 0;\
-     keyword decoration:rounding 0"
- exit
-else
- hyprctl --batch "\
-     keyword general:gaps_in 6;\
-     keyword general:gaps_out 12;\
-     keyword general:border_size 1;\
-     keyword decoration:rounding 8"
-fi
+#!/bin/bash
+
+while true; do
+    # Получаем информацию о активном рабочем пространстве
+    workspace_info=$(hyprctl activeworkspace)
+
+    # Извлекаем количество окон из информации о рабочем пространстве
+    window_count=$(echo "$workspace_info" | grep -oP 'windows:\s*\K\d+')
+
+    if (( window_count <= 1 )); then
+    hyprctl --batch "\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 0"
+    else
+    hyprctl --batch "\
+        keyword general:gaps_in 6;\
+        keyword general:gaps_out 12;\
+        keyword general:border_size 1"
+    fi
+
+    # Ждем 1 секунду перед следующей проверкой
+    sleep 0.5
+done
