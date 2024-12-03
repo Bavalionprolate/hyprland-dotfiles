96a267101003cde0a5c05a66d1e87c618591ecf0
Bavalionprolate <codethegit@gmail.com>
2024-12-03 21:20:40 +0800

    Change path main scss style

--- ignis/config.py
+++ ignis/config.py
@@ -5,7 +5,7 @@ from ignis.app import IgnisApp
 from ignis.services.hyprland import HyprlandService
 
 app = IgnisApp.get_default()
-app.apply_css(f"{Utils.get_current_dir()}/style.scss")
+app.apply_css(f"{Utils.get_current_dir()}/scss/style.scss")
 
 from modules.bar import bar
 
